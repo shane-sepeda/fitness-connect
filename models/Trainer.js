b98a7ef27c0710933f1fc241ab65f937d2ea48f3
salem z <salem.zerezghi2@gmail.com>
2022-06-25 09:47:02 -0600

    copied to dev

---
+++ models/Trainer.js
@@ -0,0 +1,65 @@
+
+const { Model, DataTypes } = require("sequelize");
+const bcrypt = require("bcrypt");
+const sequelize = require("../config/connection");
+
+class Trainer extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
+
+Trainer.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isUser_name: true,
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4],
+      },
+    },
+
+    skills: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    client_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "client[]",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "trainer",
+  }
+);
+
+module.exports = Client;
