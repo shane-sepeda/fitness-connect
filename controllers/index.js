1916c4e79fe525371afb057ea03324f0d12bfcfc
salem z <salem.zerezghi2@gmail.com>
2022-06-25 09:52:36 -0600

    fix test

---
+++ controllers/index.js
@@ -0,0 +1,12 @@
+const User = require('./Client');
+const Gallery = require('./Trainer');
+
+Trainer.hasMany(Client, {
+  foreignKey: 'client_id',
+});
+
+Client.belongsTo(Trainer, {
+  foreignKey: 'client_id',
+});
+
+module.exports = { Client, Trainer };
