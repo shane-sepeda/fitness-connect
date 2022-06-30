6b323530c9e66130054aeaef2b1e6db8afa3bd9d
salem z <salem.zerezghi2@gmail.com>
2022-06-30 16:49:18 -0600

    homeroutes

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,9 @@
+const router = require("express").Router();
 
-const router = require('express').Router();
+const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes");
 
-const apiRoutes = require('./api');
-//const homeRoutes = require('./home-routes.js');
-
-//router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
+router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
 
 module.exports = router;
