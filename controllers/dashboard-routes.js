0a7ac2ba2b2ad5588b2420192cb776d6fec06ca2
salem z <salem.zerezghi2@gmail.com>
2022-07-01 21:53:13 -0600

    just pushing

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+
+router.get('/', (req,res) => {
+    
+    // if (req.session.logged) {
+    //     res.render('dashboard', {
+    //         logged: req.session.logged
+    //     })
+    //     return;
+    // };
+    res.render('dashboard')
+  });
+
+  module.exports = router;
